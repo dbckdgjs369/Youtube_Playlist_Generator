e6ff7f49dd4d9fd8769c7d29a0322b47a2716fc9
yoo chang heon <dbckdgjs1130@naver.com>
2022-09-22 21:10:26 +0900

    style: 로그인 페이지 emotion으로 변경

--- youtube_playlist_generator/src/index.tsx
+++ youtube_playlist_generator/src/index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
@@ -12,14 +11,12 @@ const root = ReactDOM.createRoot(
 );
 const queryClient = new QueryClient();
 root.render(
-  // <React.StrictMode>
   <CreateUserProvider>
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={true} />
       <App />
     </QueryClientProvider>
   </CreateUserProvider>
-  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- youtube_playlist_generator/src/pages/LoginPage/LoginPage.tsx
+++ youtube_playlist_generator/src/pages/LoginPage/LoginPage.tsx
@@ -1,4 +1,35 @@
-import "./style.scss";
+import styled from "@emotion/styled";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 30px;
+  justify-content: center;
+`;
+
+const LoginBtn = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  width: 300px;
+  height: 50px;
+  font-size: 20px;
+  background-color: white;
+  border: none;
+  box-shadow: 1px 1px 2px gray;
+  color: gray;
+  font-weight: 300;
+  cursor: pointer;
+  a {
+    text-decoration: none;
+    color: gray;
+  }
+  &:hover {
+    background-color: #eee;
+  }
+`;
 
 const API_END_POINT = "https://accounts.google.com/o/oauth2/auth";
 const REDIRECT_URI = "http://localhost:3000/create";
@@ -8,15 +39,14 @@ const url = `${API_END_POINT}?access_type=offline&client_id=${process.env.REACT_
 console.log(url);
 export default function LoginPage() {
   return (
-    <div className="Wrapper">
-      <img src="./youtube.png" width="50px" />
+    <Wrapper>
+      <img src="./images/youtube.png" width="50px" />
       <h1>Welcome to YouTube Playlist Generator!!</h1>
       <h2>Log in to the account you want to create a playlist for </h2>
-
-      <button className="LoginBtn">
-        <img src="./google.png" width="30px" />
+      <LoginBtn>
+        <img src="./images/google.png" width="30px" />
         <a href={url}>Sign in with Google </a>
-      </button>
-    </div>
+      </LoginBtn>
+    </Wrapper>
   );
 }
