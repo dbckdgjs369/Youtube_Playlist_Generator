7ca093354a39e8b34fbb7d00360b8758686b1b80
Yoo Chang Heon <dbckdgjs1130@naver.com>
2023-01-01 23:30:33 +0900

    refactor: 시간 없애는 로직 변경

--- youtube_playlist_generator/src/utils/removeTime.ts
+++ youtube_playlist_generator/src/utils/removeTime.ts
@@ -1,9 +1,12 @@
+// const makeList = (songList: string[]) => {
+//   return songList.map((value: string) =>
+//     value
+//       .replace(/ *(\s{0,})([0-5][0-9]):([0-5][0-9])(:[0-5][0-9])*/g, "")
+//       .trim()
+//   );
+// };
 const makeList = (songList: string[]) => {
-  return songList.map((value: string) =>
-    value
-      .replace(/ *(\s{0,})([0-5][0-9]):([0-5][0-9])(:[0-5][0-9])*/g, "")
-      .trim()
-  );
+  return songList.map((value) => value.split(" ").slice(1).join(""));
 };
 
 export { makeList };
