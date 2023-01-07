2aa21298775bd11bd8aa81379c73477b91ea5b68
Yoo Chang Heon <dbckdgjs1130@naver.com>
2023-01-07 18:28:27 +0900

    style: 모달 스타일 추가

--- youtube_playlist_generator/src/components/Modal/Modal.tsx
+++ youtube_playlist_generator/src/components/Modal/Modal.tsx
@@ -29,15 +29,20 @@ const ModalBody = styled.div`
   height: 500px;
   padding: 40px;
   text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   background-color: rgb(255, 255, 255);
   border-radius: 10px;
   box-shadow: 0 2px 3px 0 rgba(34, 36, 38, 0.15);
   overflow: auto;
 `;
 const InputWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
+  position: fixed;
+  bottom: 100px;
+  left: 50%;
+  transform: translate(-50%, 0%);
 `;
 const InputDiv = styled.div`
   display: flex;
@@ -55,6 +60,26 @@ const StyledInput = styled.input`
 
 const StyledLabel = styled.label`
   ${text.$body1}
+`;
+const ContentDiv = styled.div`
+  overflow-y: scroll;
+  width: 80%;
+  height: 80%;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  margin-bottom: 2rem;
+  ::-webkit-scrollbar {
+    width: 8px;
+  }
+  ::-webkit-scrollbar-thumb {
+    background-color: #2f3542;
+    border-radius: 8px;
+  }
+  ::-webkit-scrollbar-track {
+    background-color: grey;
+    border-radius: 8px;
+  }
 `;
 
 const Modal = ({ setModalOpen, songInfoArr, accessToken }: ModalProps) => {
@@ -75,6 +100,9 @@ const Modal = ({ setModalOpen, songInfoArr, accessToken }: ModalProps) => {
       window.scrollTo(0, parseInt(scrollY || "0", 10) * -1);
     };
   }, []);
+  useEffect(() => {
+    console.log(songInfoArr);
+  }, []);
   const createPlayList = async () => {
     let playlistId = "";
     if (inputRef.current !== null) {
@@ -83,7 +111,6 @@ const Modal = ({ setModalOpen, songInfoArr, accessToken }: ModalProps) => {
       const data = createNewPlayList(playlistTitle, accessToken);
       playlistId = (await data).data.id;
     }
-
     for (const id of songInfoArr) {
       await addToPlayList(accessToken, playlistId, id.items[0].id.videoId);
     }
@@ -93,15 +120,50 @@ const Modal = ({ setModalOpen, songInfoArr, accessToken }: ModalProps) => {
     <ModalWrapper onClick={closeModal}>
       <ModalBody onClick={(e) => e.stopPropagation()}>
         {songInfoArr.length === 0 ? (
-          <p>선택된게 없네요</p>
+          <p style={{ fontWeight: 600, fontSize: "20px" }}>곡을 선택해주세요</p>
         ) : (
           <>
-            {songInfoArr.map((e) => (
+            <ContentDiv>
               <SongItem
-                src={e.items[0].snippet.thumbnails.default.url}
-                title={e.items[0].snippet.title.replaceAll("&#39;", "'")}
+                src="https://i.ytimg.com/vi/zAbjLWrphHM/default.jpg"
+                title="1asdgasdgasdgasdgasdgasdgsadgsadgsadgsadgsdag"
+                vid="1"
               />
-            ))}
+              <SongItem
+                src="https://i.ytimg.com/vi/zAbjLWrphHM/default.jpg"
+                title="1"
+                vid="1"
+              />
+              <SongItem
+                src="https://i.ytimg.com/vi/zAbjLWrphHM/default.jpg"
+                title="1"
+                vid="1"
+              />
+              <SongItem
+                src="https://i.ytimg.com/vi/zAbjLWrphHM/default.jpg"
+                title="1"
+                vid="1"
+              />
+              <SongItem
+                src="https://i.ytimg.com/vi/zAbjLWrphHM/default.jpg"
+                title="1"
+                vid="1"
+              />
+              <SongItem
+                src="https://i.ytimg.com/vi/zAbjLWrphHM/default.jpg"
+                title="1"
+                vid="1"
+              />
+            </ContentDiv>
+            {/* <ContentDiv>
+              {songInfoArr.map((e) => (
+                <SongItem
+                  src={e.items[0].snippet.thumbnails.default.url}
+                  title={e.items[0].snippet.title.replaceAll("&#39;", "'")}
+                  vid={e.items[0].id.videoId}
+                />
+              ))}
+            </ContentDiv> */}
             <InputWrapper>
               <StyledLabel>플레이리스트 제목을 입력해주세요</StyledLabel>
               <InputDiv>
